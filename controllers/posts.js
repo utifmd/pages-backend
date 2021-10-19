7b57552be3eb706bc5207c7255b54fa05e6b5cf2
Utif Milkedori <utifmd@Utifs-MacBook-Pro.local>
2021-10-19 08:14:56 +0700

    Make it better

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,5 @@
 import PostScholar from "../models/postScholar.js"
+import mongoose from "mongoose"
 
 export const getPosts = async (req, resp) => {
     try {
@@ -21,4 +22,27 @@ export const createPosts = async (req, resp) => {
     } catch (error) {
         resp.status(409).json({message: error.message})
     }
+}
+
+export const updatePosts = async (req, resp) => {
+    const { id: _id } = req.params
+    const body = req.body
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) 
+        return resp.status(404).send(`No post with id ${_id}`)
+    
+    const updated = await PostScholar.findByIdAndUpdate(_id, body, { new: true })
+
+    resp.json(updated)
+}
+
+export const deletePosts = async (req, resp) => {
+    const { id } = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)) 
+        return resp.status(404).send(`No post with id ${id}`)
+    
+    await PostScholar.findByIdAndRemove(id)
+
+    resp.json({message: 'Post deleted successfully.'})
 }
